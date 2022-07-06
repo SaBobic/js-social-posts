1977e9270b24787ca6cab0bfb7c4467091b90a21
SaBobic <fabrizioettori@gmail.com>
2022-07-06 14:45:54 +0200

    created posts array

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// # Milestone 1
+
+const posts = [
+
+    {
+        idPost: 1,
+        nameProfile: "Lucas Sinclair",
+        imgProfile: "https://picsum.photos/300/300",
+        datePost: "01-01-1986",
+        textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPost: "https://picsum.photos/300/300",
+        likesPost: "5490"
+    },
+    {
+        idPost: 2,
+        nameProfile: "Maxine Mayfield",
+        imgProfile: "https://picsum.photos/300/300",
+        datePost: "02-03-1986",
+        textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPost: "https://picsum.photos/300/300",
+        likesPost: "5936"
+    },
+    {
+        idPost: 3,
+        nameProfile: "Michael Wheeler",
+        imgProfile: "https://picsum.photos/300/300",
+        datePost: "03-05-1986",
+        textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPost: "https://picsum.photos/300/300",
+        likesPost: "9456"
+    },
+    {
+        idPost: 4,
+        nameProfile: "Jane Ives",
+        imgProfile: "https://picsum.photos/300/300",
+        datePost: "07-09-1986",
+        textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPost: "https://picsum.photos/300/300",
+        likesPost: "4853"
+    },
+    {
+        idPost: 5,
+        nameProfile: "Dustin Henderson",
+        imgProfile: "https://picsum.photos/300/300",
+        datePost: "08-01-1986",
+        textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPost: "https://picsum.photos/300/300",
+        likesPost: "5473"
+    },
+
+]
