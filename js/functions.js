8757af858f1b3cb17a1786a7fd68a7f972e8194d
SaBobic <fabrizioettori@gmail.com>
2022-07-06 15:19:22 +0200

    added functions.js and creation card script (ms 2)

---
+++ js/functions.js
@@ -0,0 +1,52 @@
+/**
+ * Funzione per creare la card di un post, utilizzando le proprietà dell'array contenente i post
+ *
+ * @param {Object[]} arr - Array da cui prendere i parametri dei post.
+ * @param {string} pic - Immagine del profilo.
+ * @param {string} name - Nome del profilo.
+ * @param {string} date - Data di pubblicazione del post.
+ * @param {string} text - Testo del post.
+ * @param {string} img - Immagine del post.
+ * @param {string} likes - Likes del post.
+ * @return {string} La funzione restituisce la stranga HTML della card montata.
+ */
+const createCard = (arr, pic, name, date, text, img, likes) => {
+
+    let card = "";
+
+    for (const item of arr){
+        card += `
+            <div class="post">
+                <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                    <img class="profile-pic" src="${item[pic]}" alt="${item[name]}" />
+                    </div>
+                    <div class="post-meta__data">
+                    <div class="post-meta__author">${item[name]}</div>
+                    <div class="post-meta__time">${item[date]}</div>
+                    </div>
+                </div>
+                </div>
+                <div class="post__text">
+                ${item[text]}
+                </div>
+                <div class="post__image">
+                <img src="${item[img]}" alt="" />
+                </div>
+                <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                    <a class="like-button js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                    </div>
+                    <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${item[likes]}</b> persone</div>
+                </div>
+                </div>
+            </div>`;
+    }
+
+    return card;
+}
--- js/script.js
+++ js/script.js
@@ -1,51 +1,59 @@
 // # Milestone 1
+// Creiamo il nostro array di oggetti che rappresentano ciascun post.
 
 const posts = [
 
     {
         idPost: 1,
         nameProfile: "Lucas Sinclair",
-        imgProfile: "https://picsum.photos/300/300",
+        imgProfile: "https://unsplash.it/300/300?image=15",
         datePost: "01-01-1986",
         textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgPost: "https://picsum.photos/300/300",
-        likesPost: "5490"
+        imgPost: "https://unsplash.it/300/200?image=16",
+        likesPost: 5490
     },
     {
         idPost: 2,
         nameProfile: "Maxine Mayfield",
-        imgProfile: "https://picsum.photos/300/300",
+        imgProfile: "https://unsplash.it/300/300?image=17",
         datePost: "02-03-1986",
         textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgPost: "https://picsum.photos/300/300",
-        likesPost: "5936"
+        imgPost: "https://unsplash.it/300/200?image=18",
+        likesPost: 5936
     },
     {
         idPost: 3,
         nameProfile: "Michael Wheeler",
-        imgProfile: "https://picsum.photos/300/300",
+        imgProfile: "https://unsplash.it/300/300?image=19",
         datePost: "03-05-1986",
         textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgPost: "https://picsum.photos/300/300",
-        likesPost: "9456"
+        imgPost: "https://unsplash.it/300/200?image=28",
+        likesPost: 9456
     },
     {
         idPost: 4,
         nameProfile: "Jane Ives",
-        imgProfile: "https://picsum.photos/300/300",
+        imgProfile: "https://unsplash.it/300/300?image=22",
         datePost: "07-09-1986",
         textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgPost: "https://picsum.photos/300/300",
-        likesPost: "4853"
+        imgPost: "https://unsplash.it/300/200?image=23",
+        likesPost: 4853
     },
     {
         idPost: 5,
         nameProfile: "Dustin Henderson",
-        imgProfile: "https://picsum.photos/300/300",
+        imgProfile: "https://unsplash.it/300/300?image=24",
         datePost: "08-01-1986",
         textPost: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgPost: "https://picsum.photos/300/300",
-        likesPost: "5473"
+        imgPost: "https://unsplash.it/300/200?image=25",
+        likesPost: 5473
     },
 
 ]
+
+// # Milestone 2
+// Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed
+
+const listElement = document.getElementById('container');
+
+listElement.innerHTML = createCard(posts, 'imgProfile', 'nameProfile', 'datePost', 'textPost', 'imgPost', 'likesPost');
